fcf913fea268b707cc470e5c8b37a3b31a995456
Eleazar Lopez <2997765060101@ingenieria.usac.edu.gt>
2021-10-30 23:56:51 -0600

    Functions (Lower, Upper)case, stringTimes and power

--- Translator/DefaultFunctions.py
+++ Translator/DefaultFunctions.py
@@ -5,11 +5,19 @@ class DefaultFunctions():
         self.DivisionBy0 = ''
         self.stringConcat = ''
         self.stringPrint = ''
+        self.stringLowerCase = ''
+        self.stringUpperCase = ''
+        self.stringTimes = ''
+        self.numberPower = ''
         self.nuevaLinea = '\n'
         self.GenerateStringConcat()
         self.GenerateDivisionBy0()
         self.GenerateOutOfBounds()
         self.GenerateStringPrint()
+        self.GenerateLowerCase()
+        self.GenerateUpperCase()
+        self.GenerateStringTimes()
+        self.GenerateNumberPower()
 
     def GenerateStringConcat(self):
         self.stringConcat = 'func stringConcat(){' + self.nuevaLinea
@@ -42,18 +50,18 @@ class DefaultFunctions():
         self.stringConcat += '}' + self.nuevaLinea
 
     def GenerateStringPrint(self):
-        self.stringConcat += 'func stringPrint(){' + self.nuevaLinea
-        self.stringConcat += '\tT0 = SP + 0 //Get initial position of the string' + self.nuevaLinea
-        self.stringConcat += '\tT0 = STACK[int(T0)] //Obtenemos el primer caracter de la cadena' + self.nuevaLinea
-        self.stringConcat += '\tL0: //Loop tag' + self.nuevaLinea
-        self.stringConcat += '\t\tT1 = HEAP[int(T0)] //Get heap character' + self.nuevaLinea
-        self.stringConcat += '\t\tif(T1 == 36) {goto L1} //Heap character is $' + self.nuevaLinea
-        self.stringConcat += '\t\tfmt.Printf("%' + 'c", int(T1)) //Character print' + self.nuevaLinea
-        self.stringConcat += '\t\tT0 = T0 + 1 //T0 increses' + self.nuevaLinea
-        self.stringConcat += '\t\tgoto L0 //Go back to loop' + self.nuevaLinea
-        self.stringConcat += '\tL1: //Exit tag' + self.nuevaLinea
-        self.stringConcat += '\t\treturn //Go back' + self.nuevaLinea
-        self.stringConcat += '}' + self.nuevaLinea
+        self.stringPrint = 'func stringPrint(){' + self.nuevaLinea
+        self.stringPrint += '\tT0 = SP + 0 //Get initial position of the string' + self.nuevaLinea
+        self.stringPrint += '\tT0 = STACK[int(T0)] //Obtenemos el primer caracter de la cadena' + self.nuevaLinea
+        self.stringPrint += '\tL0: //Loop tag' + self.nuevaLinea
+        self.stringPrint += '\t\tT1 = HEAP[int(T0)] //Get heap character' + self.nuevaLinea
+        self.stringPrint += '\t\tif(T1 == 36) {goto L1} //Heap character is $' + self.nuevaLinea
+        self.stringPrint += '\t\tfmt.Printf("%' + 'c", int(T1)) //Character print' + self.nuevaLinea
+        self.stringPrint += '\t\tT0 = T0 + 1 //T0 increses' + self.nuevaLinea
+        self.stringPrint += '\t\tgoto L0 //Go back to loop' + self.nuevaLinea
+        self.stringPrint += '\tL1: //Exit tag' + self.nuevaLinea
+        self.stringPrint += '\t\treturn //Go back' + self.nuevaLinea
+        self.stringPrint += '}' + self.nuevaLinea
 
     def GenerateOutOfBounds(self):
         self.OutOfBounds = 'func OutOfBounds(){' + self.nuevaLinea
@@ -74,19 +82,114 @@ class DefaultFunctions():
         self.OutOfBounds += '}' + self.nuevaLinea
 
     def GenerateDivisionBy0(self):
-        self.OutOfBounds = 'func DivisionBy0(){' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 68); //D' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 105); //i' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 118); //v' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 105); //i' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 115); //s' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 105); //i' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 111); //o' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 110); //n' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 32); // ' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 98); //b' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 121); //y' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 32); // ' + self.nuevaLinea
-        self.OutOfBounds += '\tfmt.Printf("%' + 'c", 48); //0' + self.nuevaLinea
-        self.OutOfBounds += '\treturn;' + self.nuevaLinea
-        self.OutOfBounds += '}' + self.nuevaLinea
+        self.DivisionBy0 = 'func DivisionBy0(){' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 68); //D' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 105); //i' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 118); //v' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 105); //i' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 115); //s' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 105); //i' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 111); //o' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 110); //n' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 32); // ' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 98); //b' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 121); //y' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 32); // ' + self.nuevaLinea
+        self.DivisionBy0 += '\tfmt.Printf("%' + 'c", 48); //0' + self.nuevaLinea
+        self.DivisionBy0 += '\treturn;' + self.nuevaLinea
+        self.DivisionBy0 += '}' + self.nuevaLinea
+
+    def GenerateLowerCase(self):
+        self.stringLowerCase = 'func stringLowerCase(){' + self.nuevaLinea
+        self.stringLowerCase += '\tT0 = SP + 1 //Get the position of fist string in stack' + self.nuevaLinea
+        self.stringLowerCase += '\tT0 = STACK[int(T0)] //Get the heap position of first string' + self.nuevaLinea
+        self.stringLowerCase += '\tT1 = HP //Save first position of new string' + self.nuevaLinea
+        self.stringLowerCase += '\tL0: //Loop tag' + self.nuevaLinea
+        self.stringLowerCase += '\t\tT2 = HEAP[int(T0)] //Get character in heap' + self.nuevaLinea
+        self.stringLowerCase += '\t\tif(T2 == 36) {goto L2} //Heap character is $' + self.nuevaLinea
+        self.stringLowerCase += '\t\tif(T2 < 65) {goto L1} //Character < A' + self.nuevaLinea
+        self.stringLowerCase += '\t\tif(T2 > 90) {goto L1} //Character > Z' + self.nuevaLinea
+        self.stringLowerCase += '\t\tT2 = T2 + 32 //Lower case' + self.nuevaLinea
+        self.stringLowerCase += '\tL1: //No need to lower case tag' + self.nuevaLinea
+        self.stringLowerCase += '\t\tHEAP[int(HP)] = T2 //Save character into heap' + self.nuevaLinea
+        self.stringLowerCase += '\t\tHP = HP + 1 //Heap increses' + self.nuevaLinea
+        self.stringLowerCase += '\t\tT0 = T0 + 1 //T0 increses' + self.nuevaLinea
+        self.stringLowerCase += '\t\tgoto L0 //Go back to first loop' + self.nuevaLinea
+        self.stringLowerCase += '\tL2:' + self.nuevaLinea
+        self.stringLowerCase += '\t\tHEAP[int(HP)] = 36 //Add end of string to heap' + self.nuevaLinea
+        self.stringLowerCase += '\t\tHP = HP + 1 //Heap increses' + self.nuevaLinea
+        self.stringLowerCase += '\t\tT0 = SP + 0 //Get return position' + self.nuevaLinea
+        self.stringLowerCase += '\t\tSTACK[int(T0)] = T1 //Save heap position of the new string into stack' + self.nuevaLinea
+        self.stringLowerCase += '\t\treturn //Go back' + self.nuevaLinea
+        self.stringLowerCase += '}' + self.nuevaLinea
+
+    def GenerateUpperCase(self):
+        self.stringUpperCase = 'func stringUpperCase(){' + self.nuevaLinea
+        self.stringUpperCase += '\tT0 = SP + 1 //Get the position of fist string in stack' + self.nuevaLinea
+        self.stringUpperCase += '\tT0 = STACK[int(T0)] //Get the heap position of first string' + self.nuevaLinea
+        self.stringUpperCase += '\tT1 = HP //Save first position of new string' + self.nuevaLinea
+        self.stringUpperCase += '\tL0: //Loop tag' + self.nuevaLinea
+        self.stringUpperCase += '\t\tT2 = HEAP[int(T0)] //Get character in heap' + self.nuevaLinea
+        self.stringUpperCase += '\t\tif(T2 == 36) {goto L2} //Heap character is $' + self.nuevaLinea
+        self.stringUpperCase += '\t\tif(T2 < 97) {goto L1} //Character < A' + self.nuevaLinea
+        self.stringUpperCase += '\t\tif(T2 > 122) {goto L1} //Character > Z' + self.nuevaLinea
+        self.stringUpperCase += '\t\tT2 = T2 - 32 //Lower case' + self.nuevaLinea
+        self.stringUpperCase += '\tL1: //No need to lower case tag' + self.nuevaLinea
+        self.stringUpperCase += '\t\tHEAP[int(HP)] = T2 //Save character into heap' + self.nuevaLinea
+        self.stringUpperCase += '\t\tHP = HP + 1 //Heap increses' + self.nuevaLinea
+        self.stringUpperCase += '\t\tT0 = T0 + 1 //T0 increses' + self.nuevaLinea
+        self.stringUpperCase += '\t\tgoto L0 //Go back to first loop' + self.nuevaLinea
+        self.stringUpperCase += '\tL2:' + self.nuevaLinea
+        self.stringUpperCase += '\t\tHEAP[int(HP)] = 36 //Add end of string to heap' + self.nuevaLinea
+        self.stringUpperCase += '\t\tHP = HP + 1 //Heap increses' + self.nuevaLinea
+        self.stringUpperCase += '\t\tT0 = SP + 0 //Get return position' + self.nuevaLinea
+        self.stringUpperCase += '\t\tSTACK[int(T0)] = T1 //Save heap position of the new string into stack' + self.nuevaLinea
+        self.stringUpperCase += '\t\treturn //Go back' + self.nuevaLinea
+        self.stringUpperCase += '}' + self.nuevaLinea
+
+    def GenerateStringTimes(self):
+        self.stringTimes = 'func stringTimes(){' + self.nuevaLinea
+        self.stringTimes += '\tT0 = SP + 1 //Get the position of fist string in stack' + self.nuevaLinea
+        self.stringTimes += '\tT0 = STACK[int(T0)] //Get the heap position of first string' + self.nuevaLinea
+        self.stringTimes += '\tT1 = SP + 2 //Get the number position' + self.nuevaLinea
+        self.stringTimes += '\tT1 = STACK[int(T1)] //Get the number of times the string will repeat' + self.nuevaLinea
+        self.stringTimes += '\tT2 = HP //Save first position of new string' + self.nuevaLinea
+        self.stringTimes += '\tL0: //First loop tag' + self.nuevaLinea
+        self.stringTimes += '\t\tif (T1 < 1) {goto L3} //Exits the loop' + self.nuevaLinea
+        self.stringTimes += '\t\tL1: //First loop tag' + self.nuevaLinea
+        self.stringTimes += '\t\t\tT3 = HEAP[int(T0)] //Get character in heap' + self.nuevaLinea
+        self.stringTimes += '\t\t\tif(T3 == 36) {goto L2} //Heap character is $, leaves loop' + self.nuevaLinea
+        self.stringTimes += '\t\t\tHEAP[int(HP)] = T3 //Save character into heap' + self.nuevaLinea
+        self.stringTimes += '\t\t\tHP = HP + 1 //Heap increses' + self.nuevaLinea
+        self.stringTimes += '\t\t\tT0 = T0 + 1 //T0 increses' + self.nuevaLinea
+        self.stringTimes += '\t\t\tgoto L1 //Go back to string loop' + self.nuevaLinea
+        self.stringTimes += '\t\tL2:' + self.nuevaLinea
+        self.stringTimes += '\t\t\tT1 = T1 - 1' + self.nuevaLinea
+        self.stringTimes += '\t\t\tT0 = SP + 1 //Get the position of fist string in stack' + self.nuevaLinea
+        self.stringTimes += '\t\t\tT0 = STACK[int(T0)] //Get the heap position of first string' + self.nuevaLinea
+        self.stringTimes += '\t\t\tgoto L0' + self.nuevaLinea
+        self.stringTimes += '\tL3: //Exit tag' + self.nuevaLinea
+        self.stringTimes += '\t\tHEAP[int(HP)] = 36 //Add end of string to heap' + self.nuevaLinea
+        self.stringTimes += '\t\tHP = HP + 1 //Heap increses' + self.nuevaLinea
+        self.stringTimes += '\t\tT0 = SP + 0 //Get return position' + self.nuevaLinea
+        self.stringTimes += '\t\tSTACK[int(T0)] = T2 //Save heap position of the new string into stack' + self.nuevaLinea
+        self.stringTimes += '\t\treturn //Go back' + self.nuevaLinea
+        self.stringTimes += '}' + self.nuevaLinea
+
+    def GenerateNumberPower(self):
+        self.stringTimes = 'func numberPower(){' + self.nuevaLinea
+        self.stringTimes += '\tT0 = SP + 1 //Get base index' + self.nuevaLinea
+        self.stringTimes += '\tT0 = STACK[int(T0)] //Get base value' + self.nuevaLinea
+        self.stringTimes += '\tT1 = SP + 2 //Get exponent index' + self.nuevaLinea
+        self.stringTimes += '\tT1 = STACK[int(T1)] //Get exponent value' + self.nuevaLinea
+        self.stringTimes += '\tT2 = 1 //Set initial value' + self.nuevaLinea
+        self.stringTimes += '\tL0: //Loop tag' + self.nuevaLinea
+        self.stringTimes += '\t\tif(T1 < 1) {goto L1} //Completed' + self.nuevaLinea
+        self.stringTimes += '\t\tT2 = T2 * T0 //Previous value * Base' + self.nuevaLinea
+        self.stringTimes += '\t\tT1 = T1 - 1 //Base - 1' + self.nuevaLinea
+        self.stringTimes += '\t\tgoto L0 //Go back to loop' + self.nuevaLinea
+        self.stringTimes += '\tL1: //Exit tag' + self.nuevaLinea
+        self.stringTimes += '\t\tT0 = SP + 0 //Set return index' + self.nuevaLinea
+        self.stringTimes += '\t\tSTACK[int(T0)] = T2 //Set return value' + self.nuevaLinea
+        self.stringTimes += '\t\treturn //Exit function' + self.nuevaLinea
+        self.stringTimes += '}' + self.nuevaLinea
--- Translator/Header.py
+++ Translator/Header.py
@@ -1,3 +1,24 @@
+# At the moment stack looks like
+# absolute | relative | name            | variable
+# 0        |    0     | stringConcat    | return
+# 1        |    1     | stringConcat    | string 1
+# 2        |    2     | stringConcat    | string 2
+#-------------------------------------------------------
+# 3        |    0     | stringPrint     | string
+#-------------------------------------------------------
+# 4        |    0     | stringLowerCase | return
+# 5        |    1     | stringLowerCase | string
+#-------------------------------------------------------
+# 6        |    0     | stringUpperCase | return
+# 7        |    1     | stringUpperCase | string
+#-------------------------------------------------------
+# 8        |    0     | stringTimes     | return
+# 9        |    1     | stringTimes     | string
+# 10       |    2     | stringTimes     | number
+#-------------------------------------------------------
+# 11       |    0     | numberPower     | return
+# 12       |    1     | numberPower     | base
+# 13       |    2     | numberPower     | exponent
 from .DefaultFunctions import DefaultFunctions
 
 class Header():
@@ -10,7 +31,6 @@ class Header():
     def generarCodigo(self):
         self.codigo = 'package main' + self.nuevaLinea
         self.codigo += 'import ("fmt")' + self.nuevaLinea
-        self.codigo += self.nuevaLinea
         self.codigo += 'var STACK[1000000] float64' + self.nuevaLinea
         self.codigo += 'var HEAP[1000000] float64' + self.nuevaLinea
         self.codigo += 'var SP, HP float64' + self.nuevaLinea
@@ -24,5 +44,9 @@ class Header():
         defaultFunctions = DefaultFunctions()
         self.codigo += defaultFunctions.stringConcat
         self.codigo += defaultFunctions.stringPrint
+        self.codigo += defaultFunctions.stringLowerCase
+        self.codigo += defaultFunctions.stringUpperCase
+        self.codigo += defaultFunctions.stringTimes
+        self.codigo += defaultFunctions.numberPower
         self.codigo += defaultFunctions.OutOfBounds
         self.codigo += defaultFunctions.DivisionBy0
